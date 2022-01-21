c5f21e08f2e6686957a98fbf246e6b8a82443130
also <76024978+alishodikin9@users.noreply.github.com>
2022-01-21 16:06:38 +0700

    upd

---
+++ controllers/access.js
@@ -0,0 +1,56 @@
+[
+    {value: 'dashboard', label:'Dashboard'},
+    {value: 'manage-content', label:'Manage Content', 
+        children : [
+            {value: 'publish-content', label:'Publish Content'},
+            {value: 'edit-content-others', label:'Edit Content Others'},
+            {value: 'delete-content-others', label:'Delete Content Others'},
+            {value: 'add-content', label:'Add Content'},
+            {value: 'edit-content', label:'Edit Content'},
+            {value: 'delete-content', label:'Delete Content'},
+        ]
+    },
+    {value: 'general-setting', label:'General Setting'},
+    {value: 'menu-setting', label:'Menu Setting',
+        children : [
+            {value: 'add-menu', label:'Add Menu'},
+            {value: 'edit-menu', label:'Edit Menu'},
+            {value: 'delete-menu', label:'Delete Menu'}
+        ]
+    },
+    {value: 'sia-dashboard', label:'SIA Dashboard'},
+    {value: 'sia-master', label:'SIA Master',
+        children : [
+            {value: 'add-sia-master', label:'Add SIA Master'},
+            {value: 'edit-sia-master', label:'Edit SIA Master'},
+            {value: 'delete-sia-master', label:'Delete SIA Master'},
+        ]
+    },
+    {value: 'sia-activity', label:'SIA Activity', 
+        children : [
+            {value: 'sia-student-class-activity', label:'SIA Student class Activity'},
+            {value: 'sia-lesson-class-acivity', label:'SIA Lesson Class Activity'},
+            {value: 'sia-lesson-class-student-activity', label:'SIA Lesson Class Student Activity'},
+            {value: 'add-sia-lesson-class-student-activity', label:'Add SIA Lesson Class Student Activity'},
+            {value: 'sia-teacher-class-lesson-activity', label:'Teacher Class Lesson'},
+            {value: 'sia-student-class-lesson-activity', label:'Student Class Lesson'},
+            {value: 'sia-one-student-class-lesson-activity', label:'One Student Class Lesson'},
+        ]
+    },
+    {value: 'master-user', label:'Master User',
+        children : [
+            {value: 'add-master-user', label:'Add Master User'},
+            {value: 'edit-master-user', label:'Edit Master User'},
+            {value: 'delete-master-user', label:'Delete Master User'},
+            {value: 'edit-master-user-others', label:'Edit Master User Others'},
+            {value: 'delete-master-user-others', label:'Delete Master User Others'},
+        ]
+    },
+    {value: 'master-role', label:'Master Role',
+        children: [
+            {value: 'add-master-role', label:'Add Master Role'},
+            {value: 'edit-master-role', label:'Edit Master Role'},
+            {value: 'delete-master-role', label:'Delete Master Role'},
+        ]
+    }
+]
